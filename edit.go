5b38f0ee95198037a346340916a37c99389a2ceb
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-12-02 10:22:31 -0500

    Refactor code to handle adding and editing states

--- edit.go
+++ edit.go
@@ -5,11 +5,18 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-type add int
+type mode int
 
 const (
-	off add = iota
-	key
+	off mode = iota
+	adding
+	editting
+)
+
+type prompt int
+
+const (
+	key = iota
 	value
 )
 
@@ -17,9 +24,10 @@ type edit_model struct {
 	project    string
 	flashcards []Flashcard
 	cursor     int
-	add        add
-	add_key    string
-	add_value  string
+	mode       mode
+	prompt     prompt
+	key        string
+	value      string
 	delete     Flashcard
 	err        error
 }
@@ -34,97 +42,115 @@ func (e *edit_model) UpdateEdit(msg tea.Msg) (tea.Cmd, state) {
 			return tea.Quit, 0
 
 		case "up", "k":
-			switch e.add {
-			case add(off):
+			switch e.mode {
+			case mode(off):
 				if e.cursor > 0 {
 					e.cursor--
 				}
-			case add(key):
-				if len(msg.String()) == 1 {
-					e.add_key += msg.String()
-				}
-			case add(value):
-				if len(msg.String()) == 1 {
-					e.add_value += msg.String()
+			default:
+				switch e.prompt {
+
+				case prompt(key):
+					if len(msg.String()) == 1 {
+						e.key += msg.String()
+					}
+				case prompt(value):
+					if len(msg.String()) == 1 {
+						e.value += msg.String()
+					}
 				}
 			}
 
 		case "down", "j":
-			switch e.add {
-			case add(off):
+			switch e.mode {
+			case mode(off):
 				if e.cursor < len(e.flashcards)-1 {
 					e.cursor++
 				}
-			case add(key):
-				if len(msg.String()) == 1 {
-					e.add_key += msg.String()
-				}
-			case add(value):
-				if len(msg.String()) == 1 {
-					e.add_value += msg.String()
+			default:
+				switch e.prompt {
+
+				case prompt(key):
+					if len(msg.String()) == 1 {
+						e.key += msg.String()
+					}
+				case prompt(value):
+					if len(msg.String()) == 1 {
+						e.value += msg.String()
+					}
 				}
 			}
 
 		case "enter":
-			switch e.add {
-			case add(key):
-				e.add = add(value)
-			case add(value):
-				e.err = addFlashcard(e.project, e.add_key, e.add_value)
-				if e.err == nil {
-					e.flashcards, e.err = getFlashcards(e.project)
-					e.add = add(off)
-					e.add_key = ""
-					e.add_value = ""
+			switch e.mode {
+			case mode(off):
+				break
+			default:
+				switch e.prompt {
+				case prompt(key):
+					e.prompt = prompt(value)
+				case prompt(value):
+					e.err = addFlashcard(e.project, e.key, e.value)
+					if e.err == nil {
+						e.flashcards, e.err = getFlashcards(e.project)
+						e.mode = mode(off)
+						e.prompt = prompt(key)
+						e.key = ""
+						e.value = ""
+					}
 				}
 			}
 
 		case "backspace":
-			switch e.add {
-			case add(key):
-				if len(e.add_key) > 0 {
-					e.add_key = e.add_key[:len(e.add_key)-1]
-				}
-			case add(value):
-				if len(e.add_value) > 0 {
-					e.add_value = e.add_value[:len(e.add_value)-1]
+			switch e.mode {
+			case mode(off):
+				break
+			default:
+				switch e.prompt {
+
+				case prompt(key):
+					if len(e.key) > 0 {
+						e.key = e.key[:len(e.key)-1]
+					}
+				case prompt(value):
+					if len(e.value) > 0 {
+						e.value = e.value[:len(e.value)-1]
+					}
 				}
 			}
 
 		case "a":
-			switch e.add {
-			case add(off):
-				e.add = add(key)
-			case add(key):
-				e.add_key += msg.String()
-			case add(value):
-				e.add_value += msg.String()
+			switch e.mode {
+			case mode(off):
+				e.mode = mode(adding)
+				e.prompt = prompt(key)
+			case mode(adding):
+				switch e.prompt {
+
+				case prompt(key):
+					e.key += msg.String()
+				case prompt(value):
+					e.value += msg.String()
+				}
 			}
 
 		case "d":
 			switch e.delete.Key {
 			case "":
-				switch e.add {
-				case add(off):
+				switch e.mode {
+				case mode(off):
 					e.delete = e.flashcards[e.cursor]
-				case add(key):
-					if len(msg.String()) == 1 {
-						e.add_key += msg.String()
-					}
-				case add(value):
-					if len(msg.String()) == 1 {
-						e.add_value += msg.String()
-					}
-				}
-			default:
-				switch e.add {
-				case add(key):
-					if len(msg.String()) == 1 {
-						e.add_key += msg.String()
-					}
-				case add(value):
-					if len(msg.String()) == 1 {
-						e.add_value += msg.String()
+				default:
+					switch e.prompt {
+
+					case prompt(key):
+						if len(msg.String()) == 1 {
+							e.key += msg.String()
+						}
+					case prompt(value):
+						if len(msg.String()) == 1 {
+							e.value += msg.String()
+						}
 					}
 				}
 			}
@@ -132,14 +158,20 @@ func (e *edit_model) UpdateEdit(msg tea.Msg) (tea.Cmd, state) {
 		case "y":
 			switch e.delete.Key {
 			case "":
-				switch e.add {
-				case add(key):
-					if len(msg.String()) == 1 {
-						e.add_key += msg.String()
-					}
-				case add(value):
-					if len(msg.String()) == 1 {
-						e.add_value += msg.String()
+				switch e.mode {
+				case mode(off):
+					break
+				default:
+					switch e.prompt {
+
+					case prompt(key):
+						if len(msg.String()) == 1 {
+							e.key += msg.String()
+						}
+					case prompt(value):
+						if len(msg.String()) == 1 {
+							e.value += msg.String()
+						}
 					}
 				}
 			default:
@@ -152,14 +184,20 @@ func (e *edit_model) UpdateEdit(msg tea.Msg) (tea.Cmd, state) {
 		case "n":
 			switch e.delete.Key {
 			case "":
-				switch e.add {
-				case add(key):
-					if len(msg.String()) == 1 {
-						e.add_key += msg.String()
-					}
-				case add(value):
-					if len(msg.String()) == 1 {
-						e.add_value += msg.String()
+				switch e.mode {
+				case mode(off):
+					break
+				default:
+					switch e.prompt {
+
+					case prompt(key):
+						if len(msg.String()) == 1 {
+							e.key += msg.String()
+						}
+					case prompt(value):
+						if len(msg.String()) == 1 {
+							e.value += msg.String()
+						}
 					}
 				}
 			default:
@@ -167,14 +205,20 @@ func (e *edit_model) UpdateEdit(msg tea.Msg) (tea.Cmd, state) {
 			}
 
 		default:
-			switch e.add {
-			case add(key):
-				if len(msg.String()) == 1 {
-					e.add_key += msg.String()
-				}
-			case add(value):
-				if len(msg.String()) == 1 {
-					e.add_value += msg.String()
+			switch e.mode {
+			case mode(off):
+				break
+			default:
+				switch e.prompt {
+
+				case prompt(key):
+					if len(msg.String()) == 1 {
+						e.key += msg.String()
+					}
+				case prompt(value):
+					if len(msg.String()) == 1 {
+						e.value += msg.String()
+					}
 				}
 
 			}
@@ -224,7 +268,7 @@ func (e *edit_model) ViewEdit() string {
 		s += "\n"
 	}
 
-	if e.add != add(off) {
+	if e.mode == mode(adding) {
 		s += "\n"
 		s += lipgloss.NewStyle().
 			SetString("You are adding a new flashcard.").
@@ -235,16 +279,16 @@ func (e *edit_model) ViewEdit() string {
 			SetString("Key: ").
 			String()
 		s += lipgloss.NewStyle().
-			SetString(e.add_key).
-			Blink(e.add == add(key)).
+			SetString(e.key).
+			Blink(e.prompt == prompt(key)).
 			String()
 		s += "\n"
 		s += lipgloss.NewStyle().
 			SetString("Value: ").
 			String()
 		s += lipgloss.NewStyle().
-			SetString(e.add_value).
-			Blink(e.add == add(value)).
+			SetString(e.value).
+			Blink(e.prompt == prompt(value)).
 			String()
 	}
 
